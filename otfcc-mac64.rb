611add7da5604d4f2141677addbe1d9c38d0324c
Liam Huang <liamhuang0205@gmail.com>
2016-07-30 18:09:52 +0800

    otfcc-mac64 0.2.4

--- otfcc-mac64.rb
+++ otfcc-mac64.rb
@@ -5,16 +5,14 @@
 class OtfccMac64 < Formula
   desc "Parses & writes SFNT structures."
   homepage "https://github.com/caryll/otfcc/releases/"
-  url "https://github.com/caryll/otfcc/releases/download/v0.2.3/otfcc-mac64-0.2.3.tar.xz"
-  version "0.2.3"
-  sha256 "f393b14f9c42c890b8a60949b13a8f9b5c3f814daa8b18901656ccc3b866f646"
+  url "https://github.com/caryll/otfcc/releases/download/v0.2.4/otfcc-mac64-0.2.4.tar.xz"
+  version "0.2.4"
+  sha256 "d57ca35ab7e35d7fa67b157b8823fde082f08f14c4a4dbdb3e228df2235fbfa0"
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
 
-    # system "tar -Jvxf otfcc-mac64-0.2.3.tar.zx"
     bin.install "otfccbuild"
     bin.install "otfccdump"
   end
-
 end
